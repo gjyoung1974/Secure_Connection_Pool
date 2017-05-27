9235b3b82af1120d15d55c6aa4fdb797273a3cc7
gjyoung1974@gmail.com <Password1!>
2017-05-27 09:28:21 -0700

    adding unit tests

--- src/test/java/com/goyoung/util/java/jee/connectionpool/secureconnections/Secure_Connection_Pool/AppTest.java
+++ src/test/java/com/goyoung/util/java/jee/connectionpool/secureconnections/Secure_Connection_Pool/AppTest.java
@@ -1,38 +1,43 @@
 package com.goyoung.util.java.jee.connectionpool.secureconnections.Secure_Connection_Pool;
 
+import com.goyoung.util.ERPM.RestAPI.client.ErpmRestClient;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        try {
+            ErpmRestClient.post("", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertTrue(true);
     }
 }
