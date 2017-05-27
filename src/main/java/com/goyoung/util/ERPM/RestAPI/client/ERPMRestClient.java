f2578c6205b526c74b1cda0f8db6c2aee5d0b4e4
gjyoung1974@gmail.com <Password1!>
2017-05-27 07:58:30 -0700

    minor refactoring vaultedPasswordDataSourceFactory

--- src/main/java/com/goyoung/util/ERPM/RestAPI/client/ERPMRestClient.java
+++ src/main/java/com/goyoung/util/ERPM/RestAPI/client/ERPMRestClient.java
@@ -10,47 +10,52 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Properties;
+import java.util.logging.*;
 
 public class ERPMRestClient {
 
-	public static String post(String input, String operation) throws IOException {
+    private static Logger logger = Logger.getLogger(ERPMRestClient.class.getName());
 
-		// Load RestAPI username & password from config file
-		File configDir = new File(System.getProperty("catalina.base"), "conf");
-		File configFile = new File(configDir, "config.properties");
-		InputStream in = new FileInputStream(configFile);
-		Properties prop = new Properties();
 
-		try {
-			prop.load(in);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    public static String post(String input, String operation) throws IOException {
 
-		SSLUtilities.trustAllHostnames();
-		SSLUtilities.trustAllHttpsCertificates();
 
-		URL url = new URL(prop.getProperty("url") + operation);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setDoOutput(true);
-		conn.setRequestMethod("POST");
-		conn.setRequestProperty("Content-Type", "application/json");
+        // Load RestAPI username & password from config file
+        File configDir = new File(System.getProperty("catalina.base"), "conf");
+        File configFile = new File(configDir, "config.properties");
+        InputStream in = new FileInputStream(configFile);
+        Properties prop = new Properties();
 
-		OutputStream os = conn.getOutputStream();
-		os.write(input.getBytes());
-		os.flush();
+        try {
+            prop.load(in);
+        } catch (IOException e) {
+            logger.log(Level.INFO, "prop.load() threw an exception.", e);
+        }
 
-		// TODO some urlconnection Eror handling
-		if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
-			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-		}
+        SSLUtilities.trustAllHostnames();
+        SSLUtilities.trustAllHttpsCertificates();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
-		StringBuilder sb = new StringBuilder();
-		String output;
-		while ((output = br.readLine()) != null) {
-			sb.append(output);
-		}
-		return sb.toString();
-	}
+        URL url = new URL(prop.getProperty("url") + operation);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type", "application/json");
+
+        OutputStream os = conn.getOutputStream();
+        os.write(input.getBytes());
+        os.flush();
+
+        // TODO some urlconnection Eror handling
+        if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            logger.log(Level.INFO, "conn.getResponseCode() threw ", new IOException("Failed : HTTP error code : " + conn.getResponseCode()));
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+        StringBuilder sb = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+        return sb.toString();
+    }
 }
---
+++ src/main/java/com/goyoung/util/ERPM/RestAPI/client/getAuthToken.java
@@ -0,0 +1,45 @@
+package com.goyoung.util.ERPM.RestAPI.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class getAuthToken {
+
+    private static Logger logger = Logger.getLogger(ERPMRestClient.class.getName());
+
+    public static String go() throws IOException {
+
+        // Load RestAPI username & password from config file
+        File configDir = new File(System.getProperty("catalina.base"), "conf");
+        File configFile = new File(configDir, "config.properties");
+        InputStream in = new FileInputStream(configFile);
+        Properties prop = new Properties();
+
+        try {
+            prop.load(in);
+        } catch (IOException e) {
+            logger.log(Level.INFO, "prop.load() threw an exception.", e);
+        }
+
+        // compose our JSON message
+        String input = "{ \"LoginType\":1, \"Password\":\"" + prop.getProperty("password") + "\", \"Username\":\""
+                + prop.getProperty("username") + "\" }";
+        String operation = "DoLogin2";// set the operation endpoint
+
+        // post it & collect the response
+        String response = ERPMRestClient.post(input, operation);
+        JsonObject jobj = new Gson().fromJson(response, JsonObject.class);
+
+        return jobj.get("OperationMessage").getAsString();
+
+    }
+
+}
